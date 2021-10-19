f36d81fbed21d277cef944daced8505ff415d275
starmin0206 <85477102+starmin0206@users.noreply.github.com>
2021-10-19 15:41:54 +0900

    Add files via upload

---
+++ waterdats/waterdats/src/main/java/dats/waterdats/WaterdatsApplication.java
@@ -0,0 +1,13 @@
+package dats.waterdats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterdatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaterdatsApplication.class, args);
+	}
+
+}
---
+++ waterdats/waterdats/src/test/java/dats/waterdats/WaterdatsApplicationTests.java
@@ -0,0 +1,13 @@
+package dats.waterdats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaterdatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
